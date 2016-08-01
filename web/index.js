2dba9c3bf9f5d91ea2d3188ae94355729c4c7caf
Afanasy Argunov <argunov.com@gmail.com>
2016-08-01 19:25:50 +0900

    https://trello.com/c/BwIr1Oy7

--- web/index.js
+++ web/index.js
@@ -4,8 +4,15 @@ var morgan = require('morgan');
 var config = require('./config');
 var mongoose = require('./lib/mongoose');
 
+var fs = require('fs');
+var https = require('https');
+
+var key = fs.readFileSync('./cert/server.key');
+var cert = fs.readFileSync('./cert/cert.crt');
+var https_options = {key : key, cert : cert};
+
 var app = express();
-var http = require('http').Server(app);
+var server = https.Server(https_options, app);
 
 app.use(bodyParser.urlencoded({extended : true, limit : '2mb'}));
 app.use(bodyParser.json({limit : '2mb'}));
@@ -28,12 +35,20 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 } else {
-  var port = 3000;
-  http.listen(port, function(err) {
+  var port = 3001;
+  server.listen(port, function(err) {
     if (err) {
       console.log(err);
     } else {
       console.log('listening on port ' + port);
     }
   });
+
+  var http = require('http');
+  http.createServer(function(req, res) {
+        res.writeHead(
+            301, {"Location" : "https://" + req.headers['host'] + req.url});
+        res.end();
+      })
+      .listen(3000);
 }
